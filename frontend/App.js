85f10e7e03ea0231ea37e56ea62911e878fa91ec
이효원 <woojae829@gmail.com>
2026-03-05 15:23:05 +0900

    Add files via upload

---
+++ frontend/App.js
@@ -0,0 +1,44 @@
+import React from "react";
+import { NavigationContainer } from "@react-navigation/native";
+import { createNativeStackNavigator } from "@react-navigation/native-stack";
+import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
+
+import MapScreen from "./screens/MapScreen";
+import TimeSelectScreen from "./screens/TimeSelectScreen";
+import RouteSelectScreen from "./screens/RouteSelectScreen";
+
+import RankingScreen from "./screens/RankingScreen";
+import CommunityScreen from "./screens/CommunityScreen";
+import ProfileScreen from "./screens/ProfileScreen";
+
+import BottomTabBar from "./components/BottomTabBar";
+
+const Stack = createNativeStackNavigator();
+const Tab = createBottomTabNavigator();
+
+function WalkStack() {
+  return (
+    <Stack.Navigator screenOptions={{ headerShown: false }}>
+      <Stack.Screen name="Map" component={MapScreen} />
+      <Stack.Screen name="TimeSelect" component={TimeSelectScreen} />
+      <Stack.Screen name="RouteSelect" component={RouteSelectScreen} />
+    </Stack.Navigator>
+  );
+}
+
+export default function App() {
+  return (
+    <NavigationContainer>
+      <Tab.Navigator
+        screenOptions={{ headerShown: false }}
+      
+        tabBar={(props) => <BottomTabBar {...props} />}
+      >
+        <Tab.Screen name="Ranking" component={RankingScreen} />
+        <Tab.Screen name="Walk" component={WalkStack} />
+        <Tab.Screen name="Community" component={CommunityScreen} />
+        <Tab.Screen name="Profile" component={ProfileScreen} />
+      </Tab.Navigator>
+    </NavigationContainer>
+  );
+}
